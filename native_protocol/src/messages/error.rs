986bce617cf07a675514710a2b8b804a64ff50a7
alendavies <alendavies10@gmail.com>
2024-10-26 10:19:30 -0300

    fix error serialization

--- native_protocol/src/messages/error.rs
+++ native_protocol/src/messages/error.rs
@@ -19,8 +19,6 @@ pub enum ErrorCode {
     Unprepared = 0x2500,
 }
 
-// TODO: impl TryInto<u32> for ErrorCode
-
 impl ErrorCode {
     pub fn from_u32(value: u32) -> Option<ErrorCode> {
         match value {
@@ -48,12 +46,12 @@ impl ErrorCode {
     }
 }
 
-#[derive(Debug)]
-struct WriteTimeout;
-#[derive(Debug)]
-struct UnavailableException;
+#[derive(Debug, PartialEq)]
+pub struct WriteTimeout;
+#[derive(Debug, PartialEq)]
+pub struct UnavailableException;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 pub enum Error {
     /// Something unexpected happened. This indicates a server-side bug.
     ServerError(String),
@@ -78,27 +76,27 @@ impl Error {
 
         match self {
             Error::ServerError(message) => {
-                bytes.push(ErrorCode::ServerError.to_u32() as u8);
+                bytes.extend_from_slice(&ErrorCode::ServerError.to_u32().to_be_bytes());
                 bytes.extend_from_slice(message.as_bytes());
             }
             Error::WriteTimeout(message, _) => {
-                bytes.push(ErrorCode::WriteTimeout.to_u32() as u8);
+                bytes.extend_from_slice(&ErrorCode::WriteTimeout.to_u32().to_be_bytes());
                 bytes.extend_from_slice(message.as_bytes());
             }
             Error::ProtocolError(message) => {
-                bytes.push(ErrorCode::ProtocolError.to_u32() as u8);
+                bytes.extend_from_slice(&ErrorCode::ProtocolError.to_u32().to_be_bytes());
                 bytes.extend_from_slice(message.as_bytes());
             }
             Error::Overloaded(message) => {
-                bytes.push(ErrorCode::Overloaded.to_u32() as u8);
+                bytes.extend_from_slice(&ErrorCode::Overloaded.to_u32().to_be_bytes());
                 bytes.extend_from_slice(message.as_bytes());
             }
             Error::UnavailableException(message, _) => {
-                bytes.push(ErrorCode::UnavailableException.to_u32() as u8);
+                bytes.extend_from_slice(&ErrorCode::UnavailableException.to_u32().to_be_bytes());
                 bytes.extend_from_slice(message.as_bytes());
             }
             Error::IsBootstrapping(message) => {
-                bytes.push(ErrorCode::IsBootstrapping.to_u32() as u8);
+                bytes.extend_from_slice(&ErrorCode::IsBootstrapping.to_u32().to_be_bytes());
                 bytes.extend_from_slice(message.as_bytes());
             }
         }
@@ -129,5 +127,57 @@ impl Error {
             ErrorCode::IsBootstrapping => Some(Error::IsBootstrapping(message)),
             _ => None,
         }
+    }
+}
+
+#[cfg(test)]
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_error_to_bytes() {
+        let error = Error::ServerError("Server error".to_string());
+        let server_error_bytes = error.to_bytes();
+        assert_eq!(
+            server_error_bytes,
+            vec![
+                0x00, 0x00, 0x00, 0x00, 0x53, 0x65, 0x72, 0x76, 0x65, 0x72, 0x20, 0x65, 0x72, 0x72,
+                0x6f, 0x72
+            ]
+        );
+
+        let error = Error::ProtocolError("Protocol error".to_string());
+
+        let protocol_error_bytes = error.to_bytes();
+
+        assert_eq!(
+            protocol_error_bytes,
+            vec![
+                0x00, 0x00, 0x00, 0x0A, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x6f, 0x6c, 0x20, 0x65,
+                0x72, 0x72, 0x6f, 0x72
+            ]
+        );
+    }
+
+    #[test]
+    fn test_error_from_bytes() {
+        let server_error_bytes = vec![
+            0x00, 0x00, 0x00, 0x00, 0x53, 0x65, 0x72, 0x76, 0x65, 0x72, 0x20, 0x65, 0x72, 0x72,
+            0x6f, 0x72,
+        ];
+
+        let error = Error::from_bytes(&server_error_bytes).unwrap();
+
+        assert_eq!(error, Error::ServerError("Server error".to_string()));
+
+        let protocol_error_bytes = vec![
+            0x00, 0x00, 0x00, 0x0A, 0x50, 0x72, 0x6f, 0x74, 0x6f, 0x63, 0x6f, 0x6c, 0x20, 0x65,
+            0x72, 0x72, 0x6f, 0x72,
+        ];
+
+        let error = Error::from_bytes(&protocol_error_bytes).unwrap();
+
+        assert_eq!(error, Error::ProtocolError("Protocol error".to_string()));
     }
 }
