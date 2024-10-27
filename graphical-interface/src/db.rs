9f504892c0f9438ff780a50712602f95dd2d79b8
alendavies <alendavies10@gmail.com>
2024-10-27 00:10:19 -0300

    get_airports

--- graphical-interface/src/db.rs
+++ graphical-interface/src/db.rs
@@ -1,6 +1,7 @@
 use std::{thread, time::Duration};
 
 use driver::{self, CassandraClient, QueryResult};
+use native_protocol::messages::result::{result::Result, rows};
 use walkers::Position;
 
 #[derive(Debug, Clone)]
@@ -11,11 +12,64 @@ pub struct Airport {
 }
 
 /// Get the airports to display on the map.
-pub(crate) fn get_airports(/*driver: &CassandraClient*/) -> Vec<Airport> {
-    let query = "query to select all airports";
+pub(crate) fn get_airports(driver: &mut CassandraClient) -> Vec<Airport> {
+    let query = "SELECT iata, lat, lon FROM airports";
 
     dbg!("call");
     thread::sleep(Duration::from_millis(200));
+
+    /* if let QueryResult::Result(Result::Rows(res)) = driver.execute(query).unwrap() {
+        // obtener airports de las rows
+        let airports: Vec<_> = res
+            .rows_content
+            .iter()
+            .map(|record| Airport {
+                iata: record.get("iata").unwrap(),
+                position: Position::from_lat_lon(
+                    record.get("lat").unwrap(),
+                    record.get("lon").unwrap(),
+                ),
+            })
+            .collect();
+    } */
+
+    let result = driver.execute(query).unwrap();
+
+    match result {
+        QueryResult::Result(Result::Rows(res)) => {
+            // obtener airports de las rows
+            let mut airports: Vec<Airport> = Vec::new();
+
+            let rows = res.rows_content;
+
+            for row in rows {
+                let mut airport = Airport {
+                    iata: String::new(),
+                    position: Position::from_lat_lon(0.0, 0.0),
+                };
+                let iata = row.get("iata").unwrap();
+                match iata {
+                    rows::ColumnValue::Ascii(iata) => {
+                        airport.iata = iata.to_string();
+                    }
+                    _ => {}
+                }
+
+                let lat = row.get("lat").unwrap();
+                let lon = row.get("lon").unwrap();
+
+                match (lat, lon) {
+                    (rows::ColumnValue::Double(latitud), rows::ColumnValue::Double(longitud)) => {
+                        airport.position = Position::from_lat_lon(*latitud, *longitud);
+                    }
+                    _ => {}
+                }
+
+                airports.push(airport);
+            }
+        }
+        _ => {}
+    }
 
     vec![
         Airport {
--- graphical-interface/src/main.rs
+++ graphical-interface/src/main.rs
@@ -1,13 +1,61 @@
+use std::{net::Ipv4Addr, str::FromStr};
+
+use driver::CassandraClient;
+
 mod db;
 mod map;
 mod plugins;
 mod windows;
-use map::MyApp;
+// use map::MyApp;
 
-fn main() -> Result<(), eframe::Error> {
+/* fn main() -> Result<(), eframe::Error> {
     eframe::run_native(
         "Flight Tracker",
         Default::default(),
         Box::new(|cc| Ok(Box::new(MyApp::new(cc.egui_ctx.clone())))),
     )
+}
+ */
+
+fn main() {
+    let server_ip = "127.0.0.4";
+    let ip = Ipv4Addr::from_str(&server_ip).unwrap();
+
+    let mut client = CassandraClient::connect(ip).unwrap();
+    client.startup().unwrap();
+
+    let queries = vec![
+        "CREATE KEYSPACE airports WITH replication = {'class': 'SimpleStrategy', 'replication_factor': 3}",
+        "USE airports",
+        "CREATE TABLE airports (
+            iata TEXT PRIMARY KEY,
+            lat DOUBLE,
+            lon DOUBLE
+            PRIMARY KEY (iata)
+            )",
+        "INSERT INTO airports (iata, lat, lon) VALUES ('MAD', 40.5, -3.5)",
+        "INSERT INTO airports (iata, lat, lon) VALUES ('BCN', 41.3, 2.1)",
+        "INSERT INTO airports (iata, lat, lon) VALUES ('LAX', 33.9, -118.4)",
+        "INSERT INTO airports (iata, lat, lon) VALUES ('JFK', 40.6, -73.8)"
+        ];
+
+    let mut contador = 0;
+    let len = queries.len();
+    for query in queries {
+        match client.execute(&query) {
+            Ok(query_result) => {
+                contador += 1;
+                println!(
+                    "Consulta ejecutada exitosamente: {} y el resultado fue {:?}",
+                    query, query_result
+                );
+                println!("exitosas {:?}/{:?}", contador, len)
+            }
+            Err(e) => eprintln!("Error al ejecutar la consulta: {}\nError: {:?}", query, e),
+        }
+    }
+
+    let airports = db::get_airports(&mut client);
+
+    println!("Airports: {:?}", airports);
 }
