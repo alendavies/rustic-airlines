c39a768f984b1552248df3e03aa8458fddf1cab1
alendavies <alendavies10@gmail.com>
2024-11-04 14:24:45 -0300

    fix uuid type

--- query-creator/src/lib.rs
+++ query-creator/src/lib.rs
@@ -141,7 +141,7 @@ fn create_column_value_from_type(
         )),
         ColumnType::Uuid => {
             let bytes = value.as_bytes();
-            let uuid: [u8; 16] = bytes.try_into().map_err(|_| CQLError::Error)?;
+            let uuid = uuid::Uuid::from_slice(bytes).map_err(|_| CQLError::Error)?;
             Ok(ColumnValue::Uuid(uuid))
         }
         ColumnType::Varchar => Ok(ColumnValue::Varchar(value.to_string())),
