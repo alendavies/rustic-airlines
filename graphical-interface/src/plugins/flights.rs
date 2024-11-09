58e72f10beb0a85eb3b2dedd3a3c4eff41700b48
Luca Lazcano <64329820+lazcanoluca@users.noreply.github.com>
2024-11-09 17:59:20 -0500

    chore(ui): hovered style for planes

--- graphical-interface/src/plugins/flights.rs
+++ graphical-interface/src/plugins/flights.rs
@@ -42,21 +42,28 @@ impl Flight {
         // Define the size for the plane icon
         let symbol_size = Vec2::new(30.0, 30.0);
 
-        // Calculate the rectangle where the image should be drawn
-        let rect = Rect::from_center_size(screen_position.to_pos2(), symbol_size);
-
-        let image = Image::new(include_image!(
-            r"..\..\..\graphical-interface\plane-solid.svg"
-        ))
-        .fit_to_exact_size(symbol_size)
-        .rotate(self.heading.to_radians(), Vec2::splat(0.5));
-
-        ui.put(rect, image);
-
-        let symbol_size = Vec2::new(30.0, 30.0);
         let clickable_area = Rect::from_center_size(screen_position.to_pos2(), symbol_size);
 
         let response = ui.allocate_rect(clickable_area, egui::Sense::click());
+
+        // Calculate the rectangle where the image should be drawn
+        let rect = Rect::from_center_size(screen_position.to_pos2(), symbol_size);
+
+        let image = if response.hovered() {
+            Image::new(include_image!(
+                r"..\..\..\graphical-interface\plane-solid-selected.svg"
+            ))
+        } else {
+            Image::new(include_image!(
+                r"..\..\..\graphical-interface\plane-solid.svg"
+            ))
+        };
+
+        let image = image
+            .fit_to_exact_size(symbol_size)
+            .rotate(self.heading.to_radians(), Vec2::splat(0.5));
+
+        ui.put(rect, image);
 
         if response.clicked() {
             selection_state.toggle_flight_selection(&self);
