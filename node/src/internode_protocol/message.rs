208ff5c0e5bbe5744e66dbef6f74a84887ed78f7
Luca Lazcano <64329820+lazcanoluca@users.noreply.github.com>
2024-11-19 18:50:31 -0500

    chore(gossip): add content length to internode message header
    Co-authored-by: Alen Davies <alendavies@users.noreply.github.com>

--- node/src/internode_protocol/message.rs
+++ node/src/internode_protocol/message.rs
@@ -24,13 +24,18 @@ enum Opcode {
 struct InternodeHeader {
     opcode: Opcode,
     ip: Ipv4Addr,
+    length: u32,
 }
+
+const HEADER_SIZE: usize = 9;
 
 impl InternodeSerializable for InternodeHeader {
     /// ```md
     /// 0    8    16   24   32
     /// +----+----+----+----+
     /// |         ip        |
+    /// +----+----+----+----+
+    /// |  content_length   |
     /// +----+----+----+----+
     /// | op |              |
     /// +----+----+----+----+
@@ -40,6 +45,7 @@ impl InternodeSerializable for InternodeHeader {
         let mut bytes = Vec::new();
 
         bytes.extend_from_slice(&self.ip.octets());
+        bytes.extend_from_slice(&self.length.to_be_bytes());
         bytes.push(self.opcode as u8);
 
         bytes
@@ -59,6 +65,13 @@ impl InternodeSerializable for InternodeHeader {
 
         let ip = Ipv4Addr::from(ip_bytes);
 
+        let mut len_bytes = [0u8; 4];
+        cursor
+            .read_exact(&mut len_bytes)
+            .map_err(|_| InternodeMessageError)?;
+
+        let length = u32::from_be_bytes(len_bytes);
+
         let mut opcode_byte = [0u8; 1];
         cursor
             .read_exact(&mut opcode_byte)
@@ -71,7 +84,7 @@ impl InternodeSerializable for InternodeHeader {
             _ => return Err(InternodeMessageError),
         };
 
-        Ok(InternodeHeader { opcode, ip })
+        Ok(InternodeHeader { opcode, ip, length })
     }
 }
 
@@ -134,15 +147,16 @@ impl InternodeSerializable for InternodeMessage {
             InternodeMessageContent::Gossip(_) => Opcode::Gossip,
         };
 
-        let header = InternodeHeader {
-            ip: self.from,
-            opcode,
-        };
-
         let content_bytes = match &self.content {
             InternodeMessageContent::Query(internode_query) => internode_query.as_bytes(),
             InternodeMessageContent::Response(internode_response) => internode_response.as_bytes(),
             InternodeMessageContent::Gossip(gossip_message) => gossip_message.as_bytes(),
+        };
+
+        let header = InternodeHeader {
+            ip: self.from,
+            opcode,
+            length: content_bytes.len() as u32,
         };
 
         bytes.extend_from_slice(&header.as_bytes());
@@ -155,7 +169,7 @@ impl InternodeSerializable for InternodeMessage {
     fn from_bytes(bytes: &[u8]) -> Result<Self, InternodeMessageError> {
         let mut cursor = Cursor::new(bytes);
 
-        let mut header_bytes = [0u8; 5];
+        let mut header_bytes = [0u8; HEADER_SIZE];
         cursor
             .read_exact(&mut header_bytes)
             .map_err(|_| InternodeMessageError)?;
@@ -163,15 +177,22 @@ impl InternodeSerializable for InternodeMessage {
         let header =
             InternodeHeader::from_bytes(&header_bytes).map_err(|_| InternodeMessageError)?;
 
+        dbg!(&header);
+
+        let mut content_bytes = vec![0u8; header.length as usize];
+        cursor
+            .read_exact(&mut content_bytes)
+            .map_err(|_| InternodeMessageError)?;
+
         let content = match header.opcode {
             Opcode::Query => InternodeMessageContent::Query(
-                InternodeQuery::from_bytes(&bytes[5..]).map_err(|_| InternodeMessageError)?,
+                InternodeQuery::from_bytes(&content_bytes).map_err(|_| InternodeMessageError)?,
             ),
             Opcode::Response => InternodeMessageContent::Response(
-                InternodeResponse::from_bytes(&bytes[5..]).map_err(|_| InternodeMessageError)?,
+                InternodeResponse::from_bytes(&content_bytes).map_err(|_| InternodeMessageError)?,
             ),
             Opcode::Gossip => InternodeMessageContent::Gossip(
-                GossipMessage::from_bytes(&bytes[5..]).map_err(|_| InternodeMessageError)?,
+                GossipMessage::from_bytes(&content_bytes).map_err(|_| InternodeMessageError)?,
             ),
         };
 
@@ -204,13 +225,14 @@ mod tests {
         let header = InternodeHeader {
             opcode: Opcode::Query,
             ip: Ipv4Addr::new(127, 0, 0, 1),
+            length: 0,
         };
 
         let header_bytes = header.as_bytes();
 
         let mut bytes = Vec::new();
-
         bytes.extend_from_slice(&header.ip.octets());
+        bytes.extend_from_slice(&header.length.to_be_bytes());
         bytes.push(header.opcode as u8);
 
         assert_eq!(header_bytes, bytes);
@@ -221,6 +243,7 @@ mod tests {
         let header = InternodeHeader {
             opcode: Opcode::Query,
             ip: Ipv4Addr::new(127, 0, 0, 1),
+            length: 0,
         };
 
         let header_bytes = header.as_bytes();
@@ -264,6 +287,7 @@ mod tests {
         let header = InternodeHeader {
             opcode: Opcode::Query,
             ip: Ipv4Addr::new(127, 0, 0, 1),
+            length: query_bytes.len() as u32,
         };
 
         bytes.extend_from_slice(&header.as_bytes());
@@ -321,6 +345,7 @@ mod tests {
         let header = InternodeHeader {
             opcode: Opcode::Response,
             ip: Ipv4Addr::new(127, 0, 0, 1),
+            length: response_bytes.len() as u32,
         };
 
         bytes.extend_from_slice(&header.as_bytes());
