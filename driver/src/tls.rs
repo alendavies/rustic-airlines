0935288e8055ce0f5a6ddeddd5ab7d9df6b5fa53
lminervino18 <lminervino@fi.uba.ar>
2024-12-16 04:46:36 -0300

    Fix: Resolve bugs, update tests, and polish implementation

--- driver/src/tls.rs
+++ driver/src/tls.rs
@@ -3,25 +3,27 @@ use rustls::pki_types::CertificateDer;
 use rustls::{ClientConfig, RootCertStore};
 
 use std::env;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 
 fn load_root_cert(path: &Path) -> RootCertStore {
-    let cert = CertificateDer::from_pem_file(path).unwrap();
+    let cert = CertificateDer::from_pem_file(path).expect("Failed to load certificate");
     let mut certs = RootCertStore::empty();
-    certs.add(cert).unwrap();
-
+    certs
+        .add(cert)
+        .expect("Failed to add certificate to root store");
     certs
 }
 
 pub fn configure_client() -> ClientConfig {
-    let project_dir = env::var("CARGO_MANIFEST_DIR").unwrap_or_else(|_| ".".to_string());
-    let path = Path::new(&project_dir)
-        .parent()
-        .unwrap_or_else(|| Path::new("."))
-        .join("certs")
+    // Usar CARGO_MANIFEST_DIR para resolver la ruta al archivo `cert.crt`
+    let cert_path: PathBuf = Path::new(env!("CARGO_MANIFEST_DIR"))
+        .join("..") // Retroceder un nivel desde el crate actual
+        .join("certs") // Carpeta donde está el certificado
         .join("cert.crt");
-    let root_store = load_root_cert(&path);
 
+    let root_store = load_root_cert(&cert_path);
+
+    // Configurar el proveedor criptográfico
     match rustls::crypto::aws_lc_rs::default_provider().install_default() {
         Ok(_) => {}
         Err(err) => {
